0ef0615a178201c7005162bd2decaa7a5b43bc71
chromanelli <cmromanelli09@gmail.com>
2022-09-22 14:24:44 -0500

    update graphs with vsb

--- vision_analysis.py
+++ vision_analysis.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 
 def analysis():
     data = pd.read_csv("data/overall.csv")
-
+    low = data.loc[(data["ba"] < 0.243) | (data["vsb"] < 0.125)]
+    print(low)
+    
     ba_y = data["ba"].values
     vsa_x = data["vsa"].values
+    vsb_x = data["vsb"].values
     apal_x = data["apal"].values
 
     plt.scatter(vsa_x, ba_y)
@@ -15,11 +18,18 @@ def analysis():
     plt.savefig("graphs/vsa_analysis.jpeg")
     plt.close()
 
-    plt.scatter(apal_x, ba_y)
+    plt.scatter(vsb_x, ba_y)
     plt.title("Vision Score Version B")
     plt.ylabel("Batting Average")
-    plt.xlabel("APAL")
+    plt.xlabel("VSB")
     plt.savefig("graphs/vsb_analysis.jpeg")
+    plt.close()
+
+    plt.scatter(apal_x, ba_y)
+    plt.title("APAL")
+    plt.ylabel("Batting Average")
+    plt.xlabel("APAL")
+    plt.savefig("graphs/apal_analysis.jpeg")
     plt.close()
 
     data_good_ba = data.loc[data["ba"] > 0.243]
@@ -34,10 +44,27 @@ def analysis():
     plt.scatter(x_good, y_good, color="blue", label="Above League Avg BA")
     plt.scatter(x_bad, y_bad, color="red", label="Below League Avg BA")
     plt.legend()
-    plt.title("Vision Score vs APAL")
+    plt.title("Vision Score A vs APAL")
     plt.ylabel("APAL")
-    plt.xlabel("Vision Score")
-    plt.savefig("graphs/vs_apal.jpeg")
+    plt.xlabel("Vision Score A")
+    plt.savefig("graphs/vsa_apal.jpeg")
+    plt.close()
+
+    x_good = data_good_ba["vsb"].values
+    y_good = data_good_ba["apal"].values
+
+    x_bad = data_bad_ba["vsb"].values
+    y_bad = data_bad_ba["apal"].values
+
+    plt.scatter(x_good, y_good, color="blue", label="Above League Avg BA")
+    plt.scatter(x_bad, y_bad, color="red", label="Below League Avg BA")
+    plt.legend()
+    plt.title("Vision Score B vs APAL")
+    plt.ylabel("APAL")
+    plt.xlabel("Vision Score B")
+    plt.savefig("graphs/vsb_apal.jpeg")
     plt.close()
     
     return
+
+analysis()
